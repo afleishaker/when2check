34da2701329c41698f13ad5092ab06e1eec9b8dd
dodellmitchell@gmail.com <dodellmitchell@gmail.com>
2020-11-07 09:10:25 -0800

    Added firebase

--- src/App.js
+++ src/App.js
@@ -10,23 +10,14 @@ import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
 import Navbar from "./components/Navbar";
 
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
 function App() {
   return (
 	  <Switch>
 		  <Route exact path='/' component={Home} />
 		  <Layout>
-			  <Header>
-				  <Row justify="space-between" align="middle">
-					  <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-						  <Menu.Item key="1"><Link to="/">Home </Link></Menu.Item>
-						  <Menu.Item key="2"><Link to="/dashboard">Dashboard</Link></Menu.Item>
-						  <Menu.Item key="3">Create Event</Menu.Item>
-					  </Menu>
-					  <Avatar icon={<UserOutlined />} />
-				  </Row>
-			  </Header>
+			  <Navbar />
 			  <Content style={{ padding: '0 50px' }}>
 				  <Route exact path='/dashboard' component={Dashboard} />
 			  </Content>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {Avatar, Layout, Menu, Row} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+
+const { Header } = Layout;
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/">Home </Link>
-            <Link to="/about">About Us </Link>
-        </div>
+        <Header>
+            <Row justify="space-between" align="middle">
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1"><Link to="/">Home </Link></Menu.Item>
+                    <Menu.Item key="2"><Link to="/dashboard">Dashboard</Link></Menu.Item>
+                    <Menu.Item key="3">Create Event</Menu.Item>
+                </Menu>
+                <Avatar icon={<UserOutlined />} />
+            </Row>
+        </Header>
     );
 };
 
